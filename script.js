a27e9e220e357248d06d85975c28329d472d506f
Manish <manishkus8421@gmail.com>
2023-05-21 11:20:50 +0530

    Added Counting Animation to the Second Section

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
+function countAnimation(targetNumber, elementId) {
+  var currentNumber = 0;
+  var increment = Math.ceil(targetNumber / 250); // Adjust increment for faster counting
+  var intervalTime = 20; // Time between each increment in milliseconds
 
-  const checkbox = document.querySelector("#hide_checkbox");
-  let gitLogo = document.getElementById("gitlogo");
-  let footerLogo = document.getElementById("footerLogo");
-  let topLogo = document.getElementById("topLogo");
-
-  hide_checkbox.addEventListener("click", () => {
-    const body = document.body;
-
-    if (checkbox.checked) {
-      body.classList.add("dark");
-      body.classList.remove("light");
-      gitLogo.src = "images/github-dark.png";
-      footerLogo.src = "images/logo.png";
-      topLogo.src = "images/logo.png";
-      console.log("Dark Mode Active");
-    } else {
-      console.log("Light Mode Active");
-      body.classList.remove("dark");
-      body.classList.add("light");
-      gitLogo.src = "images/github-light.png";
-      footerLogo.src = "images/logo_dark.png";
-      topLogo.src = "images/logo_dark.png";
+  var interval = setInterval(function() {
+    currentNumber += increment;
+    if (currentNumber >= targetNumber) {
+      clearInterval(interval);
+      currentNumber = targetNumber; // Ensure the target number is reached exactly
     }
-  });
+    document.getElementById(elementId).textContent = currentNumber.toLocaleString() + '+'; // Update the display with the current number
+  }, intervalTime);
+
+}
+
+// Example usage
+countAnimation(100000, 'count1'); // Start count animation for the first text
+countAnimation(150, 'count2'); // Start count animation for the second text
+
+const checkbox = document.querySelector("#hide_checkbox");
+let gitLogo = document.getElementById("gitlogo");
+let footerLogo = document.getElementById("footerLogo");
+let topLogo = document.getElementById("topLogo");
+
+hide_checkbox.addEventListener("click", () => {
+  const body = document.body;
+
+  if (checkbox.checked) {
+    body.classList.add("dark");
+    body.classList.remove("light");
+    gitLogo.src = "images/github-dark.png";
+    footerLogo.src = "images/logo.png";
+    topLogo.src = "images/logo.png";
+    console.log("Dark Mode Active");
+  } else {
+    console.log("Light Mode Active");
+    body.classList.remove("dark");
+    body.classList.add("light");
+    gitLogo.src = "images/github-light.png";
+    footerLogo.src = "images/logo_dark.png";
+    topLogo.src = "images/logo_dark.png";
+  }
 });
